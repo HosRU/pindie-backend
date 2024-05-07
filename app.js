6543188ef0fa546169636863305aec0568e15221
Artemounchik <Artem.fak@list.ru>
2024-05-07 12:36:15 +0300

    Update files

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const bodyParser = require("body-parser");
-const routes = require("./routes/index");
 const cors = require("./middlewares/cors");
 const connectToDataBase = require("./database/connect");
 const express = require("express");
 const path = require("path");
+const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
 
 const PORT = 3000;
 const app = express();
@@ -14,7 +16,9 @@ app.use(
     cors,
     bodyParser.json(),
     express.static(path.join(__dirname, "public")),
-    routes,
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
 )
 
 server.listen(PORT)
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const gamesRoute = require("./games");
-const categoriesRoute = require("./categories");
-const usersRoute = require("./users");
-
-module.exports = {
-    gamesRoute,
-    categoriesRoute,
-    usersRoute
-}
