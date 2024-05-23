d0d1a97e7cd21b1197737650a786359deac7229a
Artemounchik <100020729+HosRU@users.noreply.github.com>
2024-05-23 18:47:56 +0300

    Update files

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,7 +13,7 @@ function cors(req, res, next){
         "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
     );
     
-    next();
+    next(); 
 }   
 
 module.exports = cors;
